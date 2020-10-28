0d14f26392b34e53e5d3bc44db58ada0557a32c5
Sondre Glimsdal <sondre.glimsdal@confirmit.com>
2020-10-28 17:18:35 +0100

    add HeroModel example

--- doracle/app.py
+++ doracle/app.py
@@ -8,6 +8,11 @@ import os
 import requests as req
 from flask import Flask, render_template, request, jsonify, g, redirect, url_for, send_from_directory
 from functools import lru_cache
+
+# The ML model files
+from doracle.model import HeroStats
+
+
 package_directory = os.path.dirname(os.path.abspath(__file__))
 
 # ----------------------------------------------------------------------------#
@@ -79,21 +84,43 @@ def suggest3():
 @app.route("/stats/<int:heroid>", methods=["GET"])
 def get_hero_stats(heroid):
 
+    hero_model = get_hero_stat_model()
+    win_rate = hero_model.get_winrate(heroid)
+    pick_rate = hero_model.get_pickrate(heroid)
+    best_paired_with = hero_model.get_best_paired_with_hero(heroid)
 
+    hero_name = get_heroes()[heroid]["name"]
+    best_paired_with_name = get_heroes()[best_paired_with]["name"]
 
-    return jsonify(heroid)
+    hero_stats = {
+        "hero": hero_name,
+        "win_rate": win_rate,
+        "pick_rate": pick_rate,
+        "best_paired_with": best_paired_with_name
+    }
+
+    print("hero_stats:", hero_stats)
+
+    return jsonify(hero_stats)
 
 # ----------------------------------------------------------------------------#
 # Helpers
 # ----------------------------------------------------------------------------#
-
-@lru_cache(maxsize=32)
+@lru_cache(maxsize=1)
 def get_heroes():
     print(os.getcwd())
     with open(os.path.join(package_directory, "data/heroes.json"), "r") as fp:
         heroes = json.load(fp)
     
     return heroes
+
+
+@lru_cache(maxsize=1)
+def get_hero_stat_model():
+    path_to_model = "/home/dota_oracle_user/models/herostat.pkl"
+
+    hero_stat_model = HeroStats.load(path_to_model)
+    return hero_stat_model
 
 
 if __name__ == '__main__':
